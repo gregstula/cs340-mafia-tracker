34b55fe71f669bf6480993ad2f5493666a3c4966
Greg Stula <gregstula@me.com>
2021-02-01 14:35:17 -0500

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import logo from './logo.svg';
 import {Navbar,Nav} from 'react-bootstrap';
+import {HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return (
+     return (
+    <Router>
       <div>
         <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Families</Nav.Link>
-      <Nav.Link href="#pricing">Individuals</Nav.Link>
-    </Nav>
-  </Navbar>
-  </div>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="mr-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/families">Families</Nav.Link>
+            <Nav.Link as={Link} to="/individuals">Individuals</Nav.Link>
+            </Nav>
+        </Navbar>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/individuals">
+            <Individuals />
+          </Route>
+          <Route path="/families">
+            <Families />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Families() {
+  return <h2>Families</h2>;
+}
+
+function Individuals() {
+  return <h2>Individuals</h2>;
+}
+
 
 export default App;
