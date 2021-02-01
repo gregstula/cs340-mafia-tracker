37a4d406dab8cbde01ad0f61875cb396929e65d4
Greg Stula <gregstula@me.com>
2021-02-01 14:08:00 -0500

    Add Navbar to test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
+import {Navbar,Nav} from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Families</Nav.Link>
+      <Nav.Link href="#pricing">Individuals</Nav.Link>
+    </Nav>
+  </Navbar>
+  </div>
   );
 }
 
