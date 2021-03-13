82249e8a08258134b3999c636b14b31f8aee8174
markwelc <markwelc@oregonstate.edu>
2021-03-12 17:09:28 -0800

    laws subtables work

--- src/components/serverUrl.js
+++ src/components/serverUrl.js
@@ -1,8 +1,8 @@
 // change var for local or hosted
 
 function serverUrl(entity) {
-    const baseUrl = 'https://cs340-mafia-server.herokuapp.com/' + entity;
-    // const baseUrl = 'http://localhost:8000/' + entity;
+    //const baseUrl = 'https://cs340-mafia-server.herokuapp.com/' + entity;
+    const baseUrl = 'http://localhost:8000/' + entity;
     return baseUrl;
 }
 
