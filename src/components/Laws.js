3f0dfe933e3170821725b26eeb5e4560bea12d36
markwelc <markwelc@oregonstate.edu>
2021-03-15 16:48:52 -0700

    laws subtables mostly works

--- src/components/Laws.js
+++ src/components/Laws.js
@@ -1,7 +1,7 @@
 
-import { Container, Form, Button, Row, Col, Table, Dropdown, DropdownButton, Modal } from 'react-bootstrap';
+import { Container, Form, Button/*, Row*/, Col, Table, Dropdown, DropdownButton, Modal } from 'react-bootstrap';
 import React from 'react';
-import { Fragment, useEffect, useState, useRef } from "react";
+import { Fragment, useEffect, useState/*, useRef*/ } from "react";
 import Axios from "axios";
 import axios from 'axios';
 import serverUrl from './serverUrl';
@@ -51,6 +51,8 @@ class LawForm extends React.Component {
   }
 }
 
+var showingLawBreakers = [];
+
 function Laws() {
 
   const [lawList, setlawList] = useState([]);
@@ -90,7 +92,7 @@ function Laws() {
   function DropDownLawActions(props) {
     return (
       <DropdownButton id="dropdown-item-button" title="Actions">
-        <Dropdown.Item as="button" onClick={() => null}>Show/Hide Law Breakers</Dropdown.Item>
+        <Dropdown.Item as="button" onClick={() => GetLawBreakers(props.law.lawID)}>Show/Hide Law Breakers</Dropdown.Item>
         <Dropdown.Item as={UpdateModal} law={props.law} />
         <Dropdown.Item as="button" onClick={() => deletelaw(props.law.lawID)}>Delete</Dropdown.Item>
       </DropdownButton>
@@ -148,6 +150,166 @@ function Laws() {
   }
 
 
+  function GetLawBreakers(lawID) {
+    //find the law we're looking for, adding it if it doesn't exist
+    var index = showingLawBreakers.findIndex((val) => {return val.lawID === lawID})
+    if(index < 0)
+    {
+      showingLawBreakers.push({"lawID": lawID, "lawBreakers": [], "potentialLawBreakers": []});
+      index = showingLawBreakers.length - 1;
+
+      Axios.get(baseUrl + `/getLawBreakers/${lawID}`).then(response => {
+        showingLawBreakers[index].lawBreakers = response.data;
+        //now that we're finished, rerender;
+        setTableView([]);
+      });
+    }
+    else //if it already exists, just remove it
+    {
+      showingLawBreakers.splice(index, 1);
+
+      //now that we're finished, rerender;
+      setTableView([]);
+    }
+  }
+
+  function PrintLawBreakers(props) {
+
+    var index = showingLawBreakers.findIndex((val) => {return val.lawID === props.law.lawID});
+
+    if(index < 0)
+      return null;
+
+    var breakersList = showingLawBreakers[index].lawBreakers;
+
+    return (
+      <tr>
+        <td colSpan="7">
+              <b>Law Breakers</b>
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Mafia Family</th>
+                    <th>Mafia Role</th>
+                    <th>Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    breakersList.map(breaker => (
+                      <tr>
+                        <td>{breaker.firstName + " " + breaker.lastName}</td>
+                        <td>{breaker.familyName}</td>
+                        <td>{breaker.mafiaRole}</td>
+                        <td><Button size="sm" variant="danger" type="delete" onClick={() => RemoveLawBreaker(breaker.individualID, props.law.lawID)}>Delete</Button></td>
+                      </tr>
+                    ))
+                  }
+                </tbody>
+              </Table>
+              <Form>
+                <Form.Control
+                  size="m"
+                  type="text"
+                  onChange={(input) => {
+                    if(input.target.value) {
+                      GetPotentialLawBreakers(input.target.value, index)
+                    }
+                    else {
+                      showingLawBreakers[index].potentialLawBreakers = [];
+                    }
+                  }}
+                  placeholder="Search for existing people to add to list of people who broke this law"
+                />
+              </Form>
+              <Button size="sm" type="search" onClick={() => {
+                  setTableView([]);
+                }}>Search</Button>
+              <Button size="sm" variant="danger" type="delete" onClick={() => {
+                  showingLawBreakers[index].potentialLawBreakers = [];
+                  setTableView([]);
+                }}>Clear Results</Button>
+              <PrintPotentialLawBreakers index={index}/>
+        </td>
+      </tr>
+    );
+  }
+
+  function GetPotentialLawBreakers(searchInput, index) {
+
+    Axios.get(baseUrl + `/searchPeople/${searchInput}`).then(response => {
+      showingLawBreakers[index].potentialLawBreakers = response.data;
+    });
+  }
+
+  function PrintPotentialLawBreakers(props) {
+
+    var index = props.index;
+
+    if(showingLawBreakers[index].potentialLawBreakers.length === 0)
+      return null;
+
+    return (
+      <Table bordered hover>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Mafia Family</th>
+            <th>Mafia Role</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>{
+          showingLawBreakers[index].potentialLawBreakers.map(person => {
+            return (
+              <tr>
+                <td>{person.firstName + " " + person.lastName}</td>
+                <td>{person.familyName}</td>
+                <td>{person.mafiaRole}</td>
+                <td><Button size="sm" type="submit" onClick={() => AddLawBreaker(person.individualID, showingLawBreakers[index].lawID)}>Add</Button></td>
+              </tr>
+            )
+          })
+        }</tbody>
+      </Table>
+    );
+
+  }
+
+  function AddLawBreaker(personID, lawID) {
+
+    const addLawBreakerUrl = baseUrl + `/addLawBreaker/${personID}/${lawID}`;
+
+    Axios.put(addLawBreakerUrl).then(resonse => {
+      Axios.get(baseUrl + `/getLawBreakers/${lawID}`).then(response => {
+        var index = showingLawBreakers.findIndex((val) => {return val.lawID === lawID});
+        showingLawBreakers[index].lawBreakers = response.data;
+        //now that we're finished, rerender;
+        setTableView([]);
+      });
+    });
+  }
+
+  function RemoveLawBreaker(personID, lawID) {
+
+    const removeLawBreakerUrl = baseUrl + `/removeLawBreaker/${personID}/${lawID}`;
+    Axios.delete(removeLawBreakerUrl).then((response) => {
+      //the things removed from the database, we just need to remove it from the array
+      showingLawBreakers.map((law) => {
+        var i = law.lawBreakers.findIndex((person) => {return person.personID === personID});
+        if(i >= 0)
+          law.lawBreakers.splice(i, 1);
+
+        return i;
+      });
+
+      console.log(showingLawBreakers);
+      setTableView([]); //rerender now that the thing in the array has been removed
+    });
+  }
+
+
   return (
     <Container fluid>
       <h1>Laws</h1>
@@ -167,6 +329,7 @@ function Laws() {
             lawList.map((law, index) => (
               <Fragment key={law.lawID}>
                 <LawRow law={law} />
+                <PrintLawBreakers law={law} />
               </Fragment>
             ))
           }
